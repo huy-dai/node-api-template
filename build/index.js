13f8f47b7557e55d53d91a30b54bc7506830bf27
Huy Dai <huydai@mit.edu>
2023-03-03 19:35:16 -0500

    Add node template

---
+++ build/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT;
+// define a route handler for the default home page
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript Server');
+});
+// start the Express server
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
